fc33ecfc71d5a79a32470c712a8fc4526d093215
Himanshu Nanikwal <106006151+Himanshu0298@users.noreply.github.com>
2022-09-26 14:40:07 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import About from "./About";
+import Contact from "./Contact";
+import Home from "./Home";
+import Products from "./Products";
+import SingleProduct from './SingleProduct';
+import Cart from './Cart';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/product" element={<Products/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/singleproduct" element={<SingleProduct/>} />
+        <Route path="/cart" element={<Cart/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
